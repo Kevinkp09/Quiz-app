3ae1cca2eabccb3d893404214fb118a8521977dd
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-16 16:45:04 +0530

    [16 Jan] [Quiz Game] Implementing a quiz game

--- quizapp.rb
+++ quizapp.rb
@@ -1,36 +1,39 @@
 class Question
   attr_accessor :question, :answer
+
   def initialize(question, answer)
     @question = question
     @answer = answer
   end
 end
 
-
-q1 = "What is the capital of Japan?\n a)Beijing\n b)Seoul\n c)Tokyo\n d)Bangkok"
-q2 = "Which planet is known as the Red Planet?\n a)Venus\n b)Mars\n c)Jupiter\n d)Saturn"
-q3 = "In which year did the first moon landing occur?\n a)1969\n b)1975\n c)1983\n d)1990"
-q4 = "Who wrote the play 'Romeo and Juliet'?\n a)William Shakespeare\n b)Jane Austen\n c)Charles Dickens\n d)Emily Brontë"
-q5 = "What is the largest mammal on Earth?\n a)Elephant\n b)Blue Whale\n c)Giraffe\n d)Gorilla"
-
-questions = [
-  Question.new(q1, "c"),
-  Question.new(q2, "b"),
-  Question.new(q3, "a"),
-  Question.new(q4, "a"),
-  Question.new(q5, "b")
-]
+def create_questions(data)
+  questions = []
+  data.each do |question_data|
+    questions << Question.new(question_data[:text], question_data[:answer])
+  end
+  questions
+end
 
 def run_quiz(questions, time_limit)
-  answer = ""
-  start_time = Time.now
   score = 0
-  for que in questions
-    puts que.question
-    answer = gets.chomp
-    if answer == que.answer
-      score += 1
+  start_time = Time.now
+
+  questions.each do |question|
+    correct_answer = true
+
+    while correct_answer
+      puts question.question
+      answer = gets.chomp
+
+      if answer == question.answer
+        score += 1
+        correct_answer = false
+      else
+        puts "Incorrect answer. Please try again."
+      end
     end
+
     elapsed_time = Time.now - start_time
     if elapsed_time > time_limit
       puts "Time limit exceeded! Quiz terminated."
@@ -38,7 +41,17 @@ def run_quiz(questions, time_limit)
     end
   end
 
-  puts "You got #{score}"
+  puts "Your score: #{score} "
 end
 
-run_quiz(questions, 20)
+questions_data = [
+  { text: "What is the capital of Japan?\n a)Beijing\n b)Seoul\n c)Tokyo\n d)Bangkok", answer: "c" },
+  { text: "Which planet is known as the Red Planet?\n a)Venus\n b)Mars\n c)Jupiter\n d)Saturn", answer: "b" },
+  { text: "In which year did the first moon landing occur?\n a)1969\n b)1975\n c)1983\n d)1990", answer: "a" },
+  { text: "Who wrote the play 'Romeo and Juliet'?\n a)William Shakespeare\n b)Jane Austen\n c)Charles Dickens\n d)Emily Brontë", answer: "a" },
+  { text: "What is the largest mammal on Earth?\n a)Elephant\n b)Blue Whale\n c)Giraffe\n d)Gorilla", answer: "b" }
+]
+
+questions = create_questions(questions_data)
+
+run_quiz(questions, 60)
